3605208bce9c8b64ccc6ab406bc5cf301beebf6b
kinghia <162483327+kinghia@users.noreply.github.com>
2025-04-20 23:56:47 +0700

    20/4 combat Boss_Logan

---
+++ Assets/Bosses/Boss.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    Transform player;
+
+    public bool isFlipped;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void LookAtPlayer()
+    {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if (transform.position.x > player.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        }
+        else if (transform.position.x < player.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+        }
+    }
+}
---
+++ Assets/Bosses/Boss_Run.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Boss_Run : StateMachineBehaviour
+{
+    public float speed = 2.5f;
+    public float attackRange = 3f;
+
+    Transform player;
+    Rigidbody2D rb;
+    Boss boss;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();  
+        boss = animator.GetComponent<Boss>();   
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        boss.LookAtPlayer();
+
+        Vector2 target = new Vector2(player.position.x, player.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.deltaTime);
+        rb.MovePosition(newPos);
+
+        if (Vector2.Distance(player.position, rb.position) <= attackRange)
+        {
+            animator.SetTrigger("Attack1");
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("Attack1");
+    }
+}
